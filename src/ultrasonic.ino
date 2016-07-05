0affd653d2724a656fcfc33be8435c82fb676cdc
lutz.schuelein <lutz.schuelein@campus.tu-berlin.de>
2016-07-05 18:20:02 +0200

    changed setup of ultrasonic implementation. moved to a more generic driver motivated implementation. [not tested]

--- src/ultrasonic.ino
+++ src/ultrasonic.ino
@@ -1,40 +1,18 @@
-/* Ultrasonic driver for US-015
+#include <NewPing.h>
+#include <Defines.h>
 
-Wiring:
-VCC => +5V
-GND => GND
-TRIG/ECHO => digital pin 2 (Pin 2)
-*/
+NewPing UltraSonicSensor(ULTRASONIC_PIN_TRIGGER, ULTRASONIC_PIN_ECHO, ULTRASONIC_MAX_DISTANCE);
 
-const int TRIGGER_PIN = 2;
-
-long duration, distance;
-
-void setup(){
+void setup()
+{
 	Serial.begin(9600);
 }
 
-void loop(){
-  //short LOW Pulse to guarantee correct HIGH Pulse
-	pinMode(TRIGGER_PIN, OUTPUT);
-	digitalWrite(TRIGGER_PIN, LOW);
-	delayMicroseconds(20);
-	digitalWrite(TRIGGER_PIN, HIGH);
-	delayMicroseconds(20);
-	digitalWrite(TRIGGER_PIN, LOW);
+int distance;
 
-  //input
-	pinMode(TRIGGER_PIN, INPUT);
-	duration = pulseIn(TRIGGER_PIN, HIGH);			//[ms]
-	distance = durationToDistance(duration);	//[cm]
-
-  //output data
+void loop()
+{
+	distance = UltraSonicSensor.ping_cm();
 	Serial.print(distance);
-  Serial.println();
-	delay(100);
-}
-
-long durationToDistance(long duration){
-//sound travels at a velocity of ~340 m/s = ~29ms/cm
-	return (duration / (29 * 2));
+	Serial.println();
 }
