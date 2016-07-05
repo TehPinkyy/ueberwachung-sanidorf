01b9b8231cca8bd226dcced7fbb10a166364ce75
lutz.schuelein <lutz.schuelein@campus.tu-berlin.de>
2016-07-05 18:21:20 +0200

    added Defines Header file for generel defines, like Pins, etc.

---
+++ src/Defines.h
@@ -0,0 +1,30 @@
+#ifndef Defines_h
+#define Defines_h
+
+
+// Ultrasonic
+#define ULTRASONIC_PIN_TRIGGER	2
+#define ULTRASONIC_PIN_ECHO		4
+#define ULTRASONIC_MAX_DISTANCE	500
+
+// Schwimmschalter
+#define SCHWIMMER_PIN			14
+
+// Pumpe Pegel
+#define PUMPE_PIN_PEGEL			17
+
+
+// Basic Defines
+#ifndef TRUE
+#define TRUE  1
+#endif
+ 
+#ifndef FALSE
+#define FALSE 0
+#endif
+
+
+
+
+#endif 
+//Defines_h
