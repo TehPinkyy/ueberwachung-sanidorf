6253422e0acbcf818542f0d696511089d8d502cf
lutz.schuelein <lutz.schuelein@campus.tu-berlin.de>
2016-07-05 15:15:58 +0200

    relocated ultrasonic

--- src/ultrasonic.ino
+++ src/ultrasonic.ino
@@ -3,21 +3,19 @@
 Wiring:
 VCC => +5V
 GND => GND
-TRIG => digital pin 12 (Pin 12)
-ECHO => digital pin 11 (Pin 11)
+TRIG/ECHO => digital pin 2 (Pin 2)
 */
 
-const int TRIGGER_PIN = 12;
-const int ECHO_PIN = 11;
+const int TRIGGER_PIN = 2;
+
+long duration, distance;
 
 void setup(){
 	Serial.begin(9600);
 }
 
 void loop(){
-	long = duration, distance;
-
-//short LOW Pulse to guarantee correct HIGH Pulse
+  //short LOW Pulse to guarantee correct HIGH Pulse
 	pinMode(TRIGGER_PIN, OUTPUT);
 	digitalWrite(TRIGGER_PIN, LOW);
 	delayMicroseconds(20);
@@ -25,16 +23,18 @@ void loop(){
 	delayMicroseconds(20);
 	digitalWrite(TRIGGER_PIN, LOW);
 
-//input
-	pinMode(ECHO_PIN, INPUT);
-	duration = pulseIn(ECHO_PIN, HIGH);			//[ms]
+  //input
+	pinMode(TRIGGER_PIN, INPUT);
+	duration = pulseIn(TRIGGER_PIN, HIGH);			//[ms]
 	distance = durationToDistance(duration);	//[cm]
 
+  //output data
 	Serial.print(distance);
-	delayMicroseconds(500);
+  Serial.println();
+	delay(100);
 }
 
 long durationToDistance(long duration){
 //sound travels at a velocity of ~340 m/s = ~29ms/cm
-	return (duration / (29 * 2);
+	return (duration / (29 * 2));
 }
